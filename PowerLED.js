e2ad54dea694a4e3fcf61b0a25ea7310897a42ac
matteobrusa <matteo.brusa@gmail.com>
2025-04-10 13:49:02 +0200

    Update PowerLED.js

--- PowerLED.js
+++ PowerLED.js
@@ -1,6 +1,6 @@
 /*
  * This scripts uses the built-in LED of the Shelly plug to show the current power 
  * production or consumption by changing the hue (color) of the LED. 
- * Blue: low power --> Red: high power
+ * off -> blue -> green -> yellow -> red (100%) -> magenta (125%)
  * Set the POWER_MAX const to calibrate the range of the expected power consumption.
  */
